5317043fcbef6c75fc8479290f5d2c1ea0f0f2f1
as27 <andreas@apxp.info>
2017-02-18 18:41:59 +0100

    Routing changed

--- cmd/artscollection/server/pages/app.js
+++ cmd/artscollection/server/pages/app.js
@@ -1,46 +1,46 @@
-const RenderString = Vue.component('render-string',{
+const RenderString = Vue.component('render-string', {
     template: `<div class="field">
         <label>{{ field.Name }}</label> 
         <input v-model="storage[field.Key]"></div>`,
-    created: function (){
-        if (this.storage[this.field.Key] == null ){
+    created: function () {
+        if (this.storage[this.field.Key] == null) {
             this.$set(this.storage, this.field.Key, "")
-            }
+        }
     },
     props: {
         storage: Object,
         field: Object
     }
 })
-const RenderInteger = Vue.component('render-integer',{
+const RenderInteger = Vue.component('render-integer', {
     template: `<div class="field">
     <label>{{ field.Name }}</label> 
     <input type="number" v-model="storage[field.Key]"></div>`,
-    created: function (){
-        if (this.storage[this.field.Key] == null ){
+    created: function () {
+        if (this.storage[this.field.Key] == null) {
             this.$set(this.storage, this.field.Key, 0)
-            }
+        }
     },
     props: {
         storage: Object,
         field: Object
     }
 })
-const RenderBool = Vue.component('render-bool',{
+const RenderBool = Vue.component('render-bool', {
     template: `<div class="field">
     <label>{{ field.Name }}</label>
      <input type="checkbox" v-model="storage[field.Key]"></div>`,
-    created: function (){
-        if (this.storage[this.field.Key] == null ){
+    created: function () {
+        if (this.storage[this.field.Key] == null) {
             this.$set(this.storage, this.field.Key, false)
-            }
+        }
     },
     props: {
         storage: Object,
         field: Object
     }
 })
-const RenderList = Vue.component('render-list',{
+const RenderList = Vue.component('render-list', {
     template: `<div>{{ field.Name }}: 
     <ul>
     <li v-for="(value,key) in storage[field.Key]"><input v-model="storage[field.Key][key]"></li>
@@ -51,25 +51,25 @@ const RenderList = Vue.component('render-list',{
         storage: Object,
         field: Object
     },
-    created: function (){
-        if (this.storage[this.field.Key] == null ){
+    created: function () {
+        if (this.storage[this.field.Key] == null) {
             this.$set(this.storage, this.field.Key, [])
-            }
+        }
     },
     methods: {
-        addTag: function (){
-            if (this.storage[this.field.Key] == null ){
+        addTag: function () {
+            if (this.storage[this.field.Key] == null) {
                 this.storage[this.field.Key] = [];
             }
             this.storage[this.field.Key].push("");
         }
     }
 })
-const RenderField = Vue.component('render-field',{
+const RenderField = Vue.component('render-field', {
     template: `<div>
     <component :is=field.Type :storage=storage :field=field></component></div>`,
     computed: {
-        fieldType: function(){
+        fieldType: function () {
             return this.field.Type
         }
     },
@@ -79,7 +79,7 @@ const RenderField = Vue.component('render-field',{
         bool: RenderBool,
         list: RenderList
     },
-    
+
     props: {
         field: Object,
         storage: Object
@@ -185,7 +185,7 @@ const Item = Vue.component('item', {
     </div>`,
     data: function () {
         return {
-            item: {fields:[]}
+            item: { fields: [] }
         }
     },
     methods: {
@@ -197,7 +197,7 @@ const Item = Vue.component('item', {
             );
         },
         saveData: function () {
-            this.$http.post('/collection/' + this.cid + '/' + this.iid,this.item);
+            this.$http.post('/collection/' + this.cid + '/' + this.iid, this.item);
         }
     },
     created: function () {
@@ -215,20 +215,22 @@ const routes = [
         name: 'home',
         component: Home,
         children: [
-            {
-                path: ':cid',
-                name: 'collection',
-                props: true,
-                component: Collection,
-                children: [
-                    {
-                        path: ':iid',
-                        name: 'item',
-                        props: true,
-                        component: Item
-                    }
-                ]
-            }]
+        ]
+    },
+    {
+        path: '/:cid',
+        name: 'collection',
+        props: true,
+        component: Collection,
+        children: [
+
+        ]
+    },
+    {
+        path: '/:cid/:iid',
+        name: 'item',
+        props: true,
+        component: Item
     },
 ]
 
